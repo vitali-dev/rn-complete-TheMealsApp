38305eaf0c8a0fb4c81824c9e43d094622df7e42
vitali-dev <vitali.repin@gmail.com>
2020-01-15 12:34:04 +0100

    first commit

---
+++ screens/CategoryMealsScreen.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+const CategoryMealScreen = props => {
+  return (
+    <View style={styles.screen}>
+      <Text>The Category Screen!</Text>
+      <Button
+        title='Go to Meal Details!'
+        onPress={() => {
+          props.navigation.navigate({ routeName: 'MealDetail' });
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default CategoryMealScreen;
