7b052f2b35844ba292952ed32a5ec30445254417
vitali-dev <vitali.repin@gmail.com>
2020-01-17 08:18:40 +0100

    Set Default Navigation Options & Config

--- screens/CategoryMealsScreen.jsx
+++ screens/CategoryMealsScreen.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, Platform } from 'react-native';
 
 import { CATEGORIES } from '../data/dummy-data';
+import Colors from '../constants/Colors';
 
 const CategoryMealScreen = props => {
   const catId = props.navigation.getParam('categoryId');
@@ -26,6 +27,20 @@ const CategoryMealScreen = props => {
       />
     </View>
   );
+};
+
+CategoryMealScreen.navigationOptions = navigationData => {
+  const catId = navigationData.navigation.getParam('categoryId');
+
+  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
+  return {
+    headerTitle: selectedCategory.title,
+    headerStyle: {
+      backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ' ',
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor,
+  };
 };
 
 const styles = StyleSheet.create({
