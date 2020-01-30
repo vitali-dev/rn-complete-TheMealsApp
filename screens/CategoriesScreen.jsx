70847e6b97823303f3d502e6756797f298a4de77
vitali-dev <vitali.repin@gmail.com>
2020-01-30 17:50:55 +0100

    Added State Management with Redux

--- screens/CategoriesScreen.jsx
+++ screens/CategoriesScreen.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import {
-  FlatList,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import { CATEGORIES } from '../data/dummy-data';
--- screens/FavoritesScreen.jsx
+++ screens/FavoritesScreen.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { useSelector } from 'react-redux';
 
 import HeaderButton from '../components/HeaderButton';
 import MealList from '../components/MealList';
-import { MEALS } from '../data/dummy-data';
+import DefaultText from '../components/DefaultText';
 
 const FavoritesScreen = props => {
-  const favMeals = MEALS.filter(
-    meals => meals.id === 'm1' || meals.id === 'm2'
-  );
+  const favMeals = useSelector(state => state.meals.favoriteMeals);
+
+  if (favMeals.length === 0 || !favMeals) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No Favorite meals founds, Start adding some!</DefaultText>
+      </View>
+    );
+  }
+
   return <MealList listData={favMeals} navigation={props.navigation} />;
 };
 
@@ -28,5 +37,13 @@ FavoritesScreen.navigationOptions = navData => {
     ),
   };
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default FavoritesScreen;
